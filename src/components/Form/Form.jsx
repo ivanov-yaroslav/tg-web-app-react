ea5f84764ceba89013263990d22c0dd2d027d55a
Yaroslav <ivanov.yaroslav.20029@gmail.com>
2023-11-09 23:48:17 +0200

    change form page

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,18 +4,18 @@ import './Form.css';
 
 function From(props) {
     const [country, setCountry] = useState('');
-    const [street, setStreet] = useState('');
+    const [city, setCity] = useState('');
     const [subject, setSubject] = useState('physical');
     const { tg } = useTelegram();
 
     const onSendData = useCallback(() => {
         const data = {
             country,
-            street,
+            city,
             subject,
         };
         tg.sendData(JSON.stringify(data));
-    }, [country, street, subject]);
+    }, [country, city, subject]);
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData);
@@ -26,24 +26,24 @@ function From(props) {
 
     useEffect(() => {
         tg.MainButton.setParams({
-            text: 'Отправить данные',
+            text: 'Відправити дані',
         });
     }, []);
 
     useEffect(() => {
-        if (!street || !country) {
+        if (!city || !country) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
         }
-    }, [country, street]);
+    }, [country, city]);
 
     const onChangeCountry = e => {
         setCountry(e.target.value);
     };
 
-    const onChangeStreet = e => {
-        setStreet(e.target.value);
+    const onChangeCity = e => {
+        setCity(e.target.value);
     };
 
     const onChangeSubject = e => {
@@ -54,7 +54,7 @@ function From(props) {
         <div className={'form'}>
             <h3>Введіть ваші дані</h3>
             <input className={'input'} type='text' placeholder={'Країна'} value={country} onChange={onChangeCountry} />
-            <input className={'input'} type='text' placeholder={'Вулиця'} value={street} onChange={onChangeStreet} />
+            <input className={'input'} type='text' placeholder={'Вулиця'} value={city} onChange={onChangeCity} />
             <select value={subject} onChange={onChangeSubject} className={'select'}>
                 <option value={'physical'}>Фіз. лице</option>
                 <option value={'legal'}>Юр. лице</option>
