d43f49032c0933f4a93d9b735a2bf5067a85da32
Yaroslav <ivanov.yaroslav.20029@gmail.com>
2023-11-09 22:25:30 +0200

    delte use telegram

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Header from './components/Header/Header';
 // import ProductList from './components/ProductList/ProductList';
 // import Form from './components/Form/Form';
 // import { Route, Routes } from 'react-router-dom';
-import { useTelegram } from './hooks/useTelegram';
+// import { useTelegram } from './hooks/useTelegram';
 
 function App() {
-    const { tg } = useTelegram();
+    // const { tg } = useTelegram();
 
     return (
         <div className='App'>
