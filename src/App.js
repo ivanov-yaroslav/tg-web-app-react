87cb60cc0f892eeb742fbf56b4e2ddf4d15694ad
Yaroslav <ivanov.yaroslav.20029@gmail.com>
2023-11-09 22:29:33 +0200

    change tg

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-// import { useEffect } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-// import ProductList from './components/ProductList/ProductList';
-// import Form from './components/Form/Form';
-// import { Route, Routes } from 'react-router-dom';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
+import { Route, Routes } from 'react-router-dom';
 // import { useTelegram } from './hooks/useTelegram';
+const tg = window.Telegram.WebApp;
 
 function App() {
     // const { tg } = useTelegram();
 
+    useEffect(() => {
+        tg.ready();
+    }, []);
+
     return (
         <div className='App'>
             <Header />
-            {/* 
-            <Routes>
+
+            {/* <Routes>
                 <Route index element={<ProductList />}></Route>
                 <Route path={'form'} element={<Form />}></Route>
             </Routes> */}
