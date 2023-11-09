a4e9a06ca990754a7120fd9333b72b7ad80e84f9
Yaroslav <ivanov.yaroslav.20029@gmail.com>
2023-11-09 22:35:28 +0200

    add tg ready

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import ProductList from './components/ProductList/ProductList';
 import { Route, Routes } from 'react-router-dom';
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
+    const { tg } = useTelegram();
+
+    useEffect(() => {
+        tg.ready();
+    }, [tg]);
+
     return (
         <div className='App'>
             <Header />
