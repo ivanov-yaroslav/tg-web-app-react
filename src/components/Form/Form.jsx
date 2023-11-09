dcfba70fcef492d92630ed94337cfc74c8b479ef
Yaroslav <ivanov.yaroslav.20029@gmail.com>
2023-11-09 23:17:18 +0200

    remove elements in form page

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -53,12 +53,6 @@ function From(props) {
     return (
         <div className={'form'}>
             <h3>Введіть ваші дані</h3>
-            <input className={'input'} type='text' placeholder={'Країна'} value={country} onChange={onChangeCountry} />
-            <input className={'input'} type='text' placeholder={'Вулиця'} value={street} onChange={onChangeStreet} />
-            <select value={subject} onChange={onChangeSubject} className={'select'}>
-                <option value={'physical'}>Фіз. лице</option>
-                <option value={'legal'}>Юр. лице</option>
-            </select>
         </div>
     );
 }
