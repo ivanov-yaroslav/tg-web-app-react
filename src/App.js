710551d9335022ed09de1fc3f1a9626a0b008e4b
Yaroslav <ivanov.yaroslav.20029@gmail.com>
2023-11-09 22:18:58 +0200

    clear app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import { useEffect } from 'react';
 import Header from './components/Header/Header';
-import { useTelegram } from './hooks/useTelegram';
 
 function App() {
-    const { tg } = useTelegram();
-
-    useEffect(() => {
-        tg.ready();
-    }, []);
-
     return (
         <div className='App'>
             <Header />
