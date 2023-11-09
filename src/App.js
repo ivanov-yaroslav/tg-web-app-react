e2dbe903dd54e3c20a89a3c87b9fa1dfc1e38573
Yaroslav <ivanov.yaroslav.20029@gmail.com>
2023-11-09 22:21:34 +0200

    add use effect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+// import ProductList from './components/ProductList/ProductList';
+// import Form from './components/Form/Form';
+// import { Route, Routes } from 'react-router-dom';
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
+    const { tg } = useTelegram();
+
+    useEffect(() => {
+        tg.ready();
+    }, []);
+
     return (
         <div className='App'>
             <Header />
+            {/* 
+            <Routes>
+                <Route index element={<ProductList />}></Route>
+                <Route path={'form'} element={<Form />}></Route>
+            </Routes> */}
         </div>
     );
 }
