ea7c739890425c0dacb67a0e48ff05a84f3827e0
Yaroslav <ivanov.yaroslav.20029@gmail.com>
2023-11-09 21:25:38 +0200

    remove pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import ProductList from './components/ProductList/ProductList';
-import Form from './components/Form/Form';
+// import ProductList from './components/ProductList/ProductList';
+// import Form from './components/Form/Form';
 import { Route, Routes } from 'react-router-dom';
 import { useTelegram } from './hooks/useTelegram';
 
@@ -18,8 +18,8 @@ function App() {
             <Header />
 
             <Routes>
-                <Route index element={<ProductList />}></Route>
-                <Route path={'form'} element={<Form />}></Route>
+                {/* <Route index element={<ProductList />}></Route> */}
+                {/* <Route path={'form'} element={<Form />}></Route> */}
             </Routes>
         </div>
     );
