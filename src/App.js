40617e385fd3baf3fdfd2eaf1bfcc8cbe89c88a7
Yaroslav <ivanov.yaroslav.20029@gmail.com>
2023-11-09 22:24:35 +0200

    remove use effect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 // import ProductList from './components/ProductList/ProductList';
