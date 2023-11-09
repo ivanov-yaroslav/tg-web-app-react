0c53d0cf3ed2ff567ce3e580770e7c64ea79dbcb
Yaroslav <ivanov.yaroslav.20029@gmail.com>
2023-11-09 22:10:59 +0200

    delete useState in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Header from './components/Header/Header';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
     return (
         <div className='App'>
             <Header />
+
+            <Routes>
+                <Route index element={<ProductList />}></Route>
+                <Route path={'form'} element={<Form />}></Route>
+            </Routes>
         </div>
     );
 }
